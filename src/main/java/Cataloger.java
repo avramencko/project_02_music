b9cdddfedc009de5a275d6725cdae41d3ca2e680
Your Name <your_email@whatever.com>
2019-03-18 19:14:48 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pom.xml

--- src/main/java/Cataloger.java
+++ src/main/java/Cataloger.java
@@ -12,6 +12,25 @@ public class Cataloger {
     private String path;
     private TreeMap<String, Performer> performers;
 
+    private final String HEAD = "<head>\n" +
+            "<meta charset=\"utf-8\">\n" +
+            "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+            "<title>Catalog</title>\n" +
+            "</head>\n";
+
+    private final String HTML = "<body>\n";
+    private final String HTML_END = "</body>\n";
+    private final String BODY = "<body>\n";
+    private final String BODY_END = "</body>\n";
+
+    private final String UL = "<ul type=\"none\">\n";
+    private final String UL_END = "</ul>\n";
+    private final String LI = "<li>\n";
+    private final String LI_END = "</li>\n";
+
+    private final String B = "<b>\n";
+    private final String B_END = "</b>\n";
+
     public Cataloger(String path){
         this.path = path;
         this.performers = new TreeMap<>();
@@ -40,6 +59,29 @@ public class Cataloger {
         for(Performer performer:performers.values())
             System.out.println(performer.describe());
     }
+
+    public void generateHTML(String path, String filename) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(path+"/"+filename+".html", "UTF-8");
+        String strHtml = HTML+HEAD+BODY;
+
+        for(Performer performer:performers.values()){
+            strHtml += UL+LI+B+performer.getName()+B_END+UL;
+            for(Album album : performer.getAlbums().values()){
+                strHtml += LI + B + album.getName() + B_END + UL;
+                for(Song song : album.getSongList()){
+                    strHtml += LI + song.getName() + " " + Song.secondsToDuration(song.getDuration())
+                            +"(<a href=\"file:///"+song.getPath()+"\">"+song.getPath()+"</a>)"+LI_END;
+                }
+                strHtml += UL_END + LI_END;
+            }
+            strHtml +=UL_END+ LI_END + UL_END;
+        }
+
+        strHtml += BODY_END+HTML_END;
+        writer.println(strHtml);
+        writer.close();
+    }
+
     private ArrayList<File> listFilesForFolder(File folder){
         ArrayList<File> files = new ArrayList<>();
         for(File fileEntry:folder.listFiles()){
